8cb4fb06e4faa1925621d5953553d0a446e738bc
Ben Harris <ben@megacorp.industries>
2022-07-10 11:34:31 +0000

    Update main.tf

--- provisioner/instances/main.tf
+++ provisioner/instances/main.tf
@@ -52,11 +52,7 @@ resource "aws_security_group" "sg_22_80" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  
   }
   egress {
     from_port   = 0
