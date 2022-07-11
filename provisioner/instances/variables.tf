e36d877116f44de435c9a2c3d3f54f354b926706
Ben Smith <ben@megacorp.industries>
2022-07-11 09:29:02 +0000

    Update variables.tf

--- provisioner/instances/variables.tf
+++ provisioner/instances/variables.tf
@@ -14,6 +14,6 @@ variable "environment_tag" {
 
 variable "region"{
   description = "The region Terraform deploys your instance"
-  default     = "ap-southeast-1"
+  default     = "ap-southeast-2"
 }
 
